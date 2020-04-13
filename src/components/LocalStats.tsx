b1622f2e35791177fb0385e996e778c4b19d10cb
Darko Bozhinovski <dbozhinovski@gmail.com>
2020-04-14 00:42:51 +0200

    Fixes warning box gnarliness

--- src/components/LocalStats.tsx
+++ src/components/LocalStats.tsx
@@ -8,7 +8,7 @@ import { Link } from "gatsby"
 import DataTable from './DataTable';
 
 const WarningBox = styled.div`
-  ${tw`bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 m-4`};
+  // ${tw`bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 m-4`};
   max-width: 75ch;
 
   p {
@@ -42,74 +42,65 @@ const fetchCountryData = async (coords, setCountryData, whoData, wikiData, ecdcD
   setCountryData(countryData);
 };
 
+const showWarningBox = (message) => (
+  <WarningBox>
+    <p>
+      {message}
+    </p>
+  </WarningBox>
+);
+
 const LocalStatsComponent = ({ wikiData, whoData, ecdcData, isGeolocationAvailable, isGeolocationEnabled, coords }) => {
-  if (!isGeolocationAvailable) return (
-    <div>
-      <WarningBox>
-        <p>
-          Your browser does not support Geolocation.
-        </p>
-      </WarningBox>
-    </div>
-  );
-  if (!isGeolocationEnabled) return (
-    <div>
-      <WarningBox>
-        <p>
-          Geolocation is not enabled (or it produced an error) - we can't show you the local data for your country.
-          <Link to="/manual-search"> Look it up manually?</Link>
-        </p>
-      </WarningBox>
-    </div>
-  );
-
-  const [countryData, setCountryData] = useState({
-    countryName: '',
-    wikiData: {
-      cases: 0,
-      deaths: 0,
-      recovered: 0
-    },
-    whoData: {
-      cases: 0,
-      deaths: 0,
-    },
-    ecdcData: {
-      cases: 0,
-      deaths: 0
-    },
-  });
-
-  useEffect(() => {
-    if (coords && coords !== null) {
-      fetchCountryData(coords, setCountryData, whoData, wikiData, ecdcData);
-    }
-  }, [ coords ]);
-
-  return (
-    <div>
-      <DataTable
-        title={`Local (showing data for ${countryData.countryName})`}
-        data={[
-          {
-            source: 'Wikipedia',
-            ref: 'https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory#covid19-container',
-            ...countryData.wikiData,
-          },
-          {
-            source: 'ECDC',
-            ref: 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/',
-            ...countryData.ecdcData,
-          },
-          {
-            source: 'WHO',
-            ref: 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports',
-            ...countryData.whoData,
-          }
-        ]}
-      />
-    </div>
-  );
+    const [countryData, setCountryData] = useState({
+      countryName: '',
+      wikiData: {
+        cases: 0,
+        deaths: 0,
+        recovered: 0
+      },
+      whoData: {
+        cases: 0,
+        deaths: 0,
+      },
+      ecdcData: {
+        cases: 0,
+        deaths: 0
+      },
+    });
+  
+    useEffect(() => {
+      if (coords && coords !== null) {
+        fetchCountryData(coords, setCountryData, whoData, wikiData, ecdcData);
+      }
+    }, [ coords, isGeolocationEnabled, isGeolocationAvailable ]);
+  
+    return !isGeolocationAvailable ? showWarningBox(<span>Your browser does not support Geolocation.</span>)
+          : !isGeolocationEnabled ? showWarningBox(<span>Geolocation is not enabled (or it produced an error) - we can't show you the local data for your country. <Link to="/manual-search"> Look it up manually?</Link></span>)
+          : (
+            <div>
+              <DataTable
+                title={`Local (showing data for ${countryData.countryName})`}
+                data={[
+                  {
+                    source: 'Wikipedia',
+                    ref: 'https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory#covid19-container',
+                    ...countryData.wikiData,
+                  },
+                  {
+                    source: 'ECDC',
+                    ref: 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/',
+                    ...countryData.ecdcData,
+                  },
+                  {
+                    source: 'WHO',
+                    ref: 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports',
+                    ...countryData.whoData,
+                  }
+                ]}
+              />
+            </div>
+          );
+      
 };
 
 export const LocalStats = geolocated({
